c03436e0a900257c61e6ca0c768a0773821b5e68
nythyatdora <nythyatdora@gmail.com>
2018-10-16 16:23:41 +0700

    modify form_main.cs

---
+++ project-library/frm_main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project_library
+{
+    public partial class frm_main : MetroFramework.Forms.MetroForm
+    {
+        public frm_main()
+        {
+            InitializeComponent();
+        }
+    }
+}
