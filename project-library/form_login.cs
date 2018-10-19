1a5f74e305cb6c61da00e721463e017675c44f4e
nythyatdora <nythyatdora@gmail.com>
2018-10-19 23:42:04 +0700

    Add frm_login.cs

---
+++ project-library/form_login.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project_library
+{
+    public partial class form_login : MetroFramework.Forms.MetroForm
+    {
+        public form_login()
+        {
+            InitializeComponent();
+        }
+    }
+}
