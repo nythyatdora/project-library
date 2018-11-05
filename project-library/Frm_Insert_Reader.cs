7be4a4d1acc56a0ccd83bce40fc8292c0d7517b0
nythyatdora <nythyatdora@gmail.com>
2018-11-05 15:22:43 +0700

    Modify frm_insert_reader.cs
    Add labels forecolor changes when mouse is entered and exited

--- project-library/Frm_Insert_Reader.cs
+++ project-library/Frm_Insert_Reader.cs
@@ -19,8 +19,82 @@ namespace project_library
 
         private void label_view_table_readers_Click(object sender, EventArgs e)
         {
-            Frm_Table frm_table = new Frm_Table("Readers");
-            frm_table.Show();
+
+        }
+
+        private void label_view_table_readers_MouseEnter(object sender, EventArgs e)
+        {
+            label_view_table_readers.ForeColor = Color.Green;
+        }
+
+        private void label_view_table_readers_MouseLeave(object sender, EventArgs e)
+        {
+            label_view_table_readers.ForeColor = Color.White;
+        }
+
+        private void label_upload_picture_MouseEnter(object sender, EventArgs e)
+        {
+            label_upload_picture.ForeColor = Color.Green;
+        }
+
+        private void label_upload_picture_MouseLeave(object sender, EventArgs e)
+        {
+            label_upload_picture.ForeColor = Color.White;
+        }
+
+        private void label_upload_identification_MouseEnter(object sender, EventArgs e)
+        {
+            label_upload_identification.ForeColor = Color.Green;
+        }
+
+        private void label_upload_identification_MouseLeave(object sender, EventArgs e)
+        {
+            label_upload_identification.ForeColor = Color.White;
+        }
+
+        private void label_reset_MouseEnter(object sender, EventArgs e)
+        {
+            label_reset.ForeColor = Color.Green;
+        }
+
+        private void label_reset_MouseLeave(object sender, EventArgs e)
+        {
+            label_reset.ForeColor = Color.White;
+        }
+
+        private void label_confirm_MouseEnter(object sender, EventArgs e)
+        {
+            label_confirm.ForeColor = Color.Green;
+        }
+
+        private void label_confirm_MouseLeave(object sender, EventArgs e)
+        {
+            label_confirm.ForeColor = Color.White;
+        }
+
+        private void label_close_MouseEnter(object sender, EventArgs e)
+        {
+            label_close.ForeColor = Color.Green;
+        }
+
+        private void label_close_MouseLeave(object sender, EventArgs e)
+        {
+            label_close.ForeColor = Color.White;
+        }
+
+        private void label_close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label_upload_picture_Click(object sender, EventArgs e)
+        {
+            openFileDialog_ReaderPicture.ShowDialog();
+        }
+
+        private void label_upload_identification_Click(object sender, EventArgs e)
+        {
+            openFileDialog_IdentificationID.ShowDialog();
         }
     }
 }
