82175efeb32e4df12ec70384b36b6d7120ab7b72
nythyatdora <nythyatdora@gmail.com>
2018-11-06 15:19:12 +0700

    Modify Frm_Borros.cs
    Add button stages when mouse is entered and left

---
+++ project-library/Class/GraphicInteraction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace project_library.Class
+{
+    public class GraphicInteraction
+    {
+        public static void SetColorLabelGreen(Label label)
+        {
+            label.ForeColor = Color.Green;
+        }
+
+        public static void SetColorLabelWhite(Label label)
+        {
+            label.ForeColor = Color.White;
+        }
+    }
+}
--- project-library/Program.cs
+++ project-library/Program.cs
@@ -16,7 +16,7 @@ namespace project_library
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Frm_Table());
+            Application.Run(new Frm_Borrow());
         }
     }
 }
