a92f5d2a4e13e5a7a45cf1d9dbb1a763e4f2dbd2
nythyatdora <nythyatdora@gmail.com>
2019-01-08 00:44:52 +0700

    Add Form_New_Management.cs

---
+++ project-library/Form_New_Management.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace project_library
+{
+    partial class Form_New_Management
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel_Form = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel_Top = new System.Windows.Forms.TableLayoutPanel();
+            this.Label_Title = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Picbox_Profile = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.Label_Username = new System.Windows.Forms.Label();
+            this.Label_Email_Address = new System.Windows.Forms.Label();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.Button_Register_Staff = new System.Windows.Forms.Button();
+            this.Button_Manage_Staff = new System.Windows.Forms.Button();
+            this.tableLayoutPanel_Form.SuspendLayout();
+            this.tableLayoutPanel_Top.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Picbox_Profile)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel_Form
+            // 
+            this.tableLayoutPanel_Form.BackColor = System.Drawing.Color.Black;
+            this.tableLayoutPanel_Form.ColumnCount = 3;
+            this.tableLayoutPanel_Form.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 2F));
+            this.tableLayoutPanel_Form.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 96F));
+            this.tableLayoutPanel_Form.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 2F));
+            this.tableLayoutPanel_Form.Controls.Add(this.tableLayoutPanel_Top, 1, 1);
+            this.tableLayoutPanel_Form.Controls.Add(this.tableLayoutPanel3, 1, 3);
+            this.tableLayoutPanel_Form.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel_Form.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel_Form.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel_Form.Name = "tableLayoutPanel_Form";
+            this.tableLayoutPanel_Form.RowCount = 4;
+            this.tableLayoutPanel_Form.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 2F));
+            this.tableLayoutPanel_Form.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel_Form.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 2F));
+            this.tableLayoutPanel_Form.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 66F));
+            this.tableLayoutPanel_Form.Size = new System.Drawing.Size(852, 579);
+            this.tableLayoutPanel_Form.TabIndex = 0;
+            // 
+            // tableLayoutPanel_Top
+            // 
+            this.tableLayoutPanel_Top.ColumnCount = 2;
+            this.tableLayoutPanel_Top.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 55F));
+            this.tableLayoutPanel_Top.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 45F));
+            this.tableLayoutPanel_Top.Controls.Add(this.Label_Title, 0, 0);
+            this.tableLayoutPanel_Top.Controls.Add(this.tableLayoutPanel1, 1, 0);
+            this.tableLayoutPanel_Top.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel_Top.Location = new System.Drawing.Point(17, 11);
+            this.tableLayoutPanel_Top.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel_Top.Name = "tableLayoutPanel_Top";
+            this.tableLayoutPanel_Top.RowCount = 1;
+            this.tableLayoutPanel_Top.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel_Top.Size = new System.Drawing.Size(817, 173);
+            this.tableLayoutPanel_Top.TabIndex = 0;
+            // 
+            // Label_Title
+            // 
+            this.Label_Title.AutoSize = true;
+            this.Label_Title.BackColor = System.Drawing.Color.Transparent;
+            this.Label_Title.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Label_Title.Font = new System.Drawing.Font("Bahnschrift", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Title.ForeColor = System.Drawing.Color.White;
+            this.Label_Title.Location = new System.Drawing.Point(0, 0);
+            this.Label_Title.Margin = new System.Windows.Forms.Padding(0);
+            this.Label_Title.Name = "Label_Title";
+            this.Label_Title.Size = new System.Drawing.Size(449, 173);
+            this.Label_Title.TabIndex = 110;
+            this.Label_Title.Text = "Management";
+            this.Label_Title.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 65F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35F));
+            this.tableLayoutPanel1.Controls.Add(this.Picbox_Profile, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(449, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(368, 173);
+            this.tableLayoutPanel1.TabIndex = 111;
+            // 
+            // Picbox_Profile
+            // 
+            this.Picbox_Profile.BackColor = System.Drawing.Color.Transparent;
+            this.Picbox_Profile.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Picbox_Profile.Location = new System.Drawing.Point(239, 25);
+            this.Picbox_Profile.Margin = new System.Windows.Forms.Padding(0);
+            this.Picbox_Profile.Name = "Picbox_Profile";
+            this.Picbox_Profile.Size = new System.Drawing.Size(129, 121);
+            this.Picbox_Profile.TabIndex = 15;
+            this.Picbox_Profile.TabStop = false;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.Label_Username, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.Label_Email_Address, 0, 2);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 25);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(239, 121);
+            this.tableLayoutPanel2.TabIndex = 16;
+            // 
+            // Label_Username
+            // 
+            this.Label_Username.AutoSize = true;
+            this.Label_Username.BackColor = System.Drawing.Color.Transparent;
+            this.Label_Username.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Label_Username.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Username.ForeColor = System.Drawing.Color.White;
+            this.Label_Username.Location = new System.Drawing.Point(0, 30);
+            this.Label_Username.Margin = new System.Windows.Forms.Padding(0);
+            this.Label_Username.Name = "Label_Username";
+            this.Label_Username.Size = new System.Drawing.Size(239, 30);
+            this.Label_Username.TabIndex = 13;
+            this.Label_Username.Text = "username";
+            this.Label_Username.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Label_Email_Address
+            // 
+            this.Label_Email_Address.AutoSize = true;
+            this.Label_Email_Address.BackColor = System.Drawing.Color.Transparent;
+            this.Label_Email_Address.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Label_Email_Address.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Email_Address.ForeColor = System.Drawing.Color.White;
+            this.Label_Email_Address.Location = new System.Drawing.Point(0, 60);
+            this.Label_Email_Address.Margin = new System.Windows.Forms.Padding(0);
+            this.Label_Email_Address.Name = "Label_Email_Address";
+            this.Label_Email_Address.Size = new System.Drawing.Size(239, 30);
+            this.Label_Email_Address.TabIndex = 14;
+            this.Label_Email_Address.Text = "email@address.com";
+            this.Label_Email_Address.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.Button_Register_Staff, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.Button_Manage_Staff, 1, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(17, 195);
+            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 93F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(817, 384);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // Button_Register_Staff
+            // 
+            this.Button_Register_Staff.BackColor = System.Drawing.Color.Transparent;
+            this.Button_Register_Staff.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Button_Register_Staff.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Button_Register_Staff.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.Button_Register_Staff.FlatAppearance.BorderSize = 0;
+            this.Button_Register_Staff.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Button_Register_Staff.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Button_Register_Staff.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button_Register_Staff.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button_Register_Staff.ForeColor = System.Drawing.Color.Transparent;
+            this.Button_Register_Staff.Image = global::project_library.Properties.Resources.imgButtonLeft_Round;
+            this.Button_Register_Staff.Location = new System.Drawing.Point(0, 0);
+            this.Button_Register_Staff.Margin = new System.Windows.Forms.Padding(0);
+            this.Button_Register_Staff.Name = "Button_Register_Staff";
+            this.Button_Register_Staff.Size = new System.Drawing.Size(408, 357);
+            this.Button_Register_Staff.TabIndex = 1;
+            this.Button_Register_Staff.Text = "REGISTER\r\nNEW STAFF";
+            this.Button_Register_Staff.UseVisualStyleBackColor = false;
+            this.Button_Register_Staff.Click += new System.EventHandler(this.Button_Register_Staff_Click);
+            this.Button_Register_Staff.MouseEnter += new System.EventHandler(this.Button_Register_Staff_MouseEnter);
+            this.Button_Register_Staff.MouseLeave += new System.EventHandler(this.Button_Register_Staff_MouseLeave);
+            // 
+            // Button_Manage_Staff
+            // 
+            this.Button_Manage_Staff.BackColor = System.Drawing.Color.Transparent;
+            this.Button_Manage_Staff.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Button_Manage_Staff.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Button_Manage_Staff.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.Button_Manage_Staff.FlatAppearance.BorderSize = 0;
+            this.Button_Manage_Staff.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Button_Manage_Staff.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Button_Manage_Staff.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button_Manage_Staff.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button_Manage_Staff.ForeColor = System.Drawing.Color.Transparent;
+            this.Button_Manage_Staff.Image = global::project_library.Properties.Resources.imgButtonLeft_Round;
+            this.Button_Manage_Staff.Location = new System.Drawing.Point(408, 0);
+            this.Button_Manage_Staff.Margin = new System.Windows.Forms.Padding(0);
+            this.Button_Manage_Staff.Name = "Button_Manage_Staff";
+            this.Button_Manage_Staff.Size = new System.Drawing.Size(409, 357);
+            this.Button_Manage_Staff.TabIndex = 16;
+            this.Button_Manage_Staff.Text = "MANAGE STAFF";
+            this.Button_Manage_Staff.UseVisualStyleBackColor = false;
+            this.Button_Manage_Staff.Click += new System.EventHandler(this.Button_Manage_Staff_Click);
+            this.Button_Manage_Staff.MouseEnter += new System.EventHandler(this.Button_Manage_Staff_MouseEnter);
+            this.Button_Manage_Staff.MouseLeave += new System.EventHandler(this.Button_Manage_Staff_MouseLeave);
+            // 
+            // Form_New_Management
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 579);
+            this.Controls.Add(this.tableLayoutPanel_Form);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form_New_Management";
+            this.Text = "Form_New_Book";
+            this.tableLayoutPanel_Form.ResumeLayout(false);
+            this.tableLayoutPanel_Top.ResumeLayout(false);
+            this.tableLayoutPanel_Top.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Picbox_Profile)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel_Form;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel_Top;
+        private System.Windows.Forms.Label Label_Title;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox Picbox_Profile;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label Label_Username;
+        private System.Windows.Forms.Label Label_Email_Address;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button Button_Register_Staff;
+        private System.Windows.Forms.Button Button_Manage_Staff;
+    }
+}
---
+++ project-library/Form_New_Management.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using project_library.Class;
+
+namespace project_library
+{
+    public partial class Form_New_Management : Form
+    {
+        public Form_New_Management()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Register_Staff_MouseEnter(object sender, EventArgs e)
+        {
+            GraphicInteraction.SetImageButtonEnter_Round(Button_Register_Staff);
+        }
+
+        private void Button_Register_Staff_MouseLeave(object sender, EventArgs e)
+        {
+            GraphicInteraction.SetImageButtonLeave_Round(Button_Register_Staff);
+        }
+
+        private void Button_Manage_Staff_MouseEnter(object sender, EventArgs e)
+        {
+            GraphicInteraction.SetImageButtonEnter_Round(Button_Manage_Staff);
+        }
+
+        private void Button_Manage_Staff_MouseLeave(object sender, EventArgs e)
+        {
+            GraphicInteraction.SetImageButtonLeave_Round(Button_Manage_Staff);
+        }
+
+        private void Button_Register_Staff_Click(object sender, EventArgs e)
+        {
+            ControlMethod.LoadForm(null);
+        }
+
+        private void Button_Manage_Staff_Click(object sender, EventArgs e)
+        {
+            ControlMethod.LoadForm(null);
+        }
+    }
+}
